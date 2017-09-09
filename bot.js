568e9d815d1d3304423bb8a63b8717bbaf8d3b9b
marian <marianacappo@gmail.com>
2017-09-08 22:10:27 -0300

    graph

--- bot.js
+++ bot.js
@@ -183,7 +183,7 @@ function sendGenericMessage(recipientId) {
 
 function callSendAPI(messageData) {
   request({
-    uri: 'https://graph.facebook.com/v2.6/'+messageData.recipient.id+'/messages',
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
     qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
     method: 'POST',
     json: messageData
